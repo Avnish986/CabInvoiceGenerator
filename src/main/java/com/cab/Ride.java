99701ed36abf9d92a46c26cb1aadc5b889e89ba0
avnish986 <avnishgupta986@gmail.com>
2020-10-24 00:26:21 +0530

    [Avnish]Added: calculate fare for given distance and time

--- src/main/java/com/cab/Ride.java
+++ src/main/java/com/cab/Ride.java
@@ -1,10 +1,5 @@
 package com.cab;
 
 public class Ride {
-	final double distance;
-	final int time;
-	public Ride(double distance, int time) {
-		this.distance=distance;
-		this.time=time;
-	}
+	
 }
